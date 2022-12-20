5c82e3e6ca02e6ded1eec5a7872f6e78c5afba29
Степан Бокарев <bokarevstepan1@yandex.ru>
2022-12-20 15:09:53 +0300

    Добавил secret key

--- ToDoAppFinal/ToDoAppFinal/settings.py
+++ ToDoAppFinal/ToDoAppFinal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '58b650d4-e41d-4383-a7c8-c5ddf8397fc0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
